b106d21f04227084aa4a583f37ee987fd35576c3
Fany Parama Admaja <fany@trst.co.id>
2025-01-03 09:53:21 +0700

    minor update in template

--- application/views/templates_admin/update_anggota.php
+++ application/views/templates_admin/update_anggota.php
@@ -3,11 +3,11 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Update Buku</h1>
+            <h1 class="m-0 text-dark">Update Profile</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Update Buku</a></li>
+              <li class="breadcrumb-item"><a href="#">Update Profile</a></li>
               <li class="breadcrumb-item active">Admin Page</li>
             </ol>
           </div>
